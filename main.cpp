0505fea77717e7ddb3de56c827d815b1278a75e9
oscar camazon <ocamazon@minsait.com>
2022-08-17 12:27:33 +0200

    Initial commit Alpha simplest cmake binary

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "maths.h"
+
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    int y = Double(x);
+    cout << "The answer = " << y << endl;
+    return 0;
+}
